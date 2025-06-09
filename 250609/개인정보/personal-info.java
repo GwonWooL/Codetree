4100a4ca36cdf2040788bcd9d190771f4d507806
Lee gwonwoo <143773589+229861@users.noreply.github.com>
2025-06-09 09:32:45 +0900

    Title: 개인정보, Time: 139ms, Memory: 12MB, Status: Passed - Codetree

--- 250609/개인정보/personal-info.java
+++ 250609/개인정보/personal-info.java
@@ -1,68 +1,43 @@
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Comparator;
-
-// 학생들의 정보를 나타내는 클래스 선언
 class Student {
-    String name;
-    int height;
-    double weight;
-
-    public Student(String name, int height, double weight){
+    String name; int height;  double weight;
+    public Student(String name, int height, double weight) {
         this.name = name;
         this.height = height;
         this.weight = weight;
     }
-};
-
+}
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // 변수 선언 및 입력:
-        int n = 5;
-        Student[] students = new Student[n];
-        for(int i = 0; i < n; i++) {
-            String name = sc.next();
-            int height = sc.nextInt();
-            double weight = sc.nextDouble();
-
-            // Student 객체를 생성해 리스트에 추가합니다.
-            students[i] = new Student(name, height, weight);
+        Student str[] = new Student[5];
+        for(int i = 0; i<5; i++) {
+            String a = sc.next();  int b = sc.nextInt(); double c = sc.nextDouble();
+            str[i] = new Student(a, b, c);
         }
-
-        // custom comparator를 활용한 정렬
-        Arrays.sort(students, new Comparator<Student>() {  
-            @Override
-            public int compare(Student a, Student b) { // 이름 기준 오름차순 정렬합니다.
+        Arrays.sort(str, new Comparator<Student>() {
+            public int compare(Student a, Student b) {
                 return a.name.compareTo(b.name);
             }
         });
-
-        // 이름순으로 정렬한 결과를 출력합니다.
         System.out.println("name");
-        for (int i = 0; i < n; i++){
-            System.out.print(students[i].name + " ");
-            System.out.print(students[i].height + " ");
-            System.out.printf("%.1f\n", students[i].weight);
+        for(int i = 0; i<5; i++) {
+            System.out.print(str[i].name+" "+str[i].height+" ");
+            System.out.printf("%.1f\n", str[i].weight);
         }
-
-        System.out.println();
-
-         // custom comparator를 활용한 정렬
-        Arrays.sort(students, new Comparator<Student>() {  
-            @Override
-            public int compare(Student a, Student b) { // 키 기준 내림차순 정렬합니다.
-                return b.height - a.height;
+        Arrays.sort(str, new Comparator<Student>() {
+            public int compare(Student a, Student b) {
+                return b.height-a.height;
             }
         });
-
-        // 키순으로 정렬한 결과를 출력합니다.
+        System.out.println();
         System.out.println("height");
-        for (int i = 0; i < n; i++){
-            System.out.print(students[i].name + " ");
-            System.out.print(students[i].height + " ");
-            System.out.printf("%.1f\n", students[i].weight);
+        for(int i = 0; i<5; i++) {
+            System.out.print(str[i].name+" "+str[i].height+" ");
+            System.out.printf("%.1f\n", str[i].weight);
         }
+
     }
 }
