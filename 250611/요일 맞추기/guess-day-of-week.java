21138e93df6a3c2098d0c0bffe50af367270fd3f
Lee gwonwoo <143773589+229861@users.noreply.github.com>
2025-06-11 01:04:17 +0900

    Title: 요일 맞추기, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250611/요일 맞추기/guess-day-of-week.java
+++ 250611/요일 맞추기/guess-day-of-week.java
@@ -5,7 +5,7 @@ public class Main {
         int m1 = sc.nextInt(); int d1 = sc.nextInt(); int m2 = sc.nextInt(); int d2 = sc.nextInt();
         int arr[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int day = 0; int total= 0; int sum = 0;
-        String str[] = {"Mon", "Thu", "Wed", "Thr", "Fri", "Sat", "Sun"};
+        String str[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         if(m2==m1) {
             day = d2 - d1;
             total = day;
