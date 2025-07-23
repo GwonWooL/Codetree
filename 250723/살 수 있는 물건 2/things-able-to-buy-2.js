16c53cea98d6e15411a753e78c34ec86e3ce0466
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-23 19:57:35 +0900

    Title: 살 수 있는 물건 2, Time: 179ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250723/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs")
+let money = Number(fs.readFileSync(0).toString())
+
+if(money >= 3000) {
+    console.log("book")
+}
+else if(money >= 1000) {
+    console.log("mask")
+}
+else if(money >= 500) {
+    console.log("pen")
+}
+else {
+    console.log("no")
+}
