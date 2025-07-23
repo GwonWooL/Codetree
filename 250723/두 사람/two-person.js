aaf4a5697aa4fff82dd7f404779452b55c2f5d8c
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-23 21:40:50 +0900

    Title: 두 사람, Time: 168ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250723/두 사람/two-person.js
@@ -0,0 +1,11 @@
+const fs = require("fs")
+let input = fs.readFileSync(0).toString().split("\n")
+
+let first = input[0].split(" ")
+let second = input[1].split(" ")
+let first_A = Number(first[0]), first_S = first[1]
+let second_A = Number(second[0]), second_S = second[1]
+
+if((first_A >= 19 || second_A >= 19)&&(first_S=='M')||second_S=='M') {
+    console.log(1)
+}
