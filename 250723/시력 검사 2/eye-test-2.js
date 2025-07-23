c557f5a6b9f362eff43a46cdea6cb0e36878af09
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-23 19:43:31 +0900

    Title: 시력 검사 2, Time: 204ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250723/시력 검사 2/eye-test-2.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+let eyes = Number(fs.readFileSync(0).toString())
+
+if(eyes >= 1.0) {
+    console.log("High")
+}
+else if(eyes >= 0.5) {
+    console.log("Middle")
+}
+else {
+    console.log("Low")
+}
