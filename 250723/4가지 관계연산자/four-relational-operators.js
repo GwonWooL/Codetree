ad1e206ca7e5b45b1931bb5db63e07612480e521
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-23 20:18:27 +0900

    Title: 4가지 관계연산자, Time: 183ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250723/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+let input = fs.readFileSync(0).toString().split(" ").map(Number)
+
+let a = input[0]
+let b = input[1]
+
+console.log(a >= b ? 1:0)
+console.log(a > b ? 1:0)
+console.log(b >= a ? 1:0)
+console.log(b > a ? 1:0)
