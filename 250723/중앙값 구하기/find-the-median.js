551dd9214588b25fd20b37d821f2fc6307427801
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-23 22:54:27 +0900

    Title: 중앙값 구하기, Time: 175ms, Memory: 14MB, Status: Passed - Codetree

--- 250723/중앙값 구하기/find-the-median.js
+++ 250723/중앙값 구하기/find-the-median.js
@@ -1,31 +1,10 @@
 const fs = require("fs")
 let input = fs.readFileSync(0).toString().split(' ').map(Number)
 
-let a = input[0]
-let b = input[1]
-let c = input[2]
+let arr = []
+arr.push(input[0], input[1], input[2])
 
-if(a > b) {
-    if(b > c) {
-        console.log(b)
-    }
-    else if(c > b && a > c) {
-        console.log(c)
-    }
-}
-if(b > a) {
-    if(a > c) {
-        console.log(a)
-    }
-    else if(c > a && b > c) {
-        console.log(c)
-    }
-}
-if(c > a) {
-    if(a > b) {
-        console.log(a)
-    }
-    else {
-        console.log(b)
-    }
-}
+arr.sort((a, b)=> a-b)
+let middle = parseInt((arr.length/2))
+
+console.log(arr[middle])
