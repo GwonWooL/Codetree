04a4fa3d2fc3f1c8249de8271c91ef6fca6c9a4b
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-24 22:23:28 +0900

    Title: 완전수 판별기, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250724/완전수 판별기/perfect-number-discriminator.js
@@ -0,0 +1,15 @@
+const fs = require("fs")
+let n = Number(fs.readFileSync(0).toString())
+let sum = 0;
+
+for(let i = 1; i<n; i++) {
+    if(n%i==0) {
+        sum += i
+    }
+}
+if(sum==n) {
+    console.log("P")
+}
+else {
+    console.log("N")
+}
