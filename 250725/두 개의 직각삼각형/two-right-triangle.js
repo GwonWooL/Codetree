433062cb9a1f391741fc5078f605a7d780119437
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 17:57:55 +0900

    Title: 두 개의 직각삼각형, Time: 187ms, Memory: 15MB, Status: Passed - Codetree

--- 250725/두 개의 직각삼각형/two-right-triangle.js
+++ 250725/두 개의 직각삼각형/two-right-triangle.js
@@ -4,10 +4,16 @@ let n = Number(fs.readFileSync(0).toString())
 for(let i = 0; i<n; i++) {
     let star = ""
     for(let j=i; j<n; j++) {
-        star+"*"
+        star+="*"
     }
     for(let k = 0; k<i; k++) {
         star+=" "
+    }
+    for(let k = 0; k<i; k++) {
+        star+=" "
+    }
+    for(let j=i; j<n; j++) {
+        star+="*"
     }
     console.log(star)
 }
