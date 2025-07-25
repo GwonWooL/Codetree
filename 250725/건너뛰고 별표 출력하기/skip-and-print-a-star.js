35ec8775ebdefb4f9f3af72bda1feb4806c7f306
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 17:14:17 +0900

    Title: 건너뛰고 별표 출력하기, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250725/건너뛰고 별표 출력하기/skip-and-print-a-star.js
@@ -0,0 +1,17 @@
+const fs = require("fs")
+let n = Number(fs.readFileSync(0).toString())
+
+for(let i = 0; i<n; i++) {
+    let star = ""
+    for(let k = 0; k<=i; k++) {
+        star +="*"
+    }
+    console.log(`${star}\n`)
+}
+for(let i = n-1; i>0; i--) {
+    let star =""
+    for(let k =i; k>0; k--) {
+        star+="*"
+    }
+    console.log(`${star}\n`)
+}
