2c7ebfac06cc4a24bc39acaa9c4befc20fea2d1d
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 16:53:39 +0900

    Title: 별표 출력하기 5, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250725/별표 출력하기 5/print-star-5.js
+++ 250725/별표 출력하기 5/print-star-5.js
@@ -1,9 +1,10 @@
 const fs = require("fs")
 let n = Number(fs.readFileSync(0).toString())
-let star = ""
+
 
 for(let i = n; i>0; i--) {
-    for(let j = 0; j<n; j++) {
+    let star = ""
+    for(let j = i; j>0; j--) {
         for(let k = i; k>0; k--) {
             star +="*"
         }
