c8bdc9d85fad1171d42d1ec68ed28ee27d614154
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 19:34:42 +0900

    Title: 체크 다이아몬드, Time: 167ms, Memory: 15MB, Status: Passed - Codetree

--- 250725/체크 다이아몬드/check-diamond.js
+++ 250725/체크 다이아몬드/check-diamond.js
@@ -11,3 +11,13 @@ for(let i = 0; i<n; i++) {
     }
     console.log(star)
 }
+for(let i = n-1; i>0; i--) {
+    let star = ""
+    for(let j = i; j<n; j++){
+        star += " "
+    }
+    for(let k = i; k>0; k--) {
+        star+="* "
+    }
+    console.log(star)
+}
