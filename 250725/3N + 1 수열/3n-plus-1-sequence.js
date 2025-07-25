b874510429d228ec22b41d958bf1e6dd86eac775
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 15:02:32 +0900

    Title: 3N + 1 수열, Time: 166ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250725/3N + 1 수열/3n-plus-1-sequence.js
@@ -0,0 +1,18 @@
+const fs = require("fs")
+let n = Number(fs.readFileSync(0).toString())
+let count = 0;
+
+while(true) {
+    if(n==1) {
+        console.log(count)
+        break
+    }
+    if(n%2==0) {
+        n/=2
+        count++
+    }
+    else {
+        n = n*3+1
+        count++
+    }
+}
