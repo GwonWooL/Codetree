8b9cfda10e4b4fe5867e400b2df4e63d4096103e
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 14:49:23 +0900

    Title: 20대의 평균 나이, Time: 5045ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

--- 250725/20대의 평균 나이/average-age-of-20.js
+++ 250725/20대의 평균 나이/average-age-of-20.js
@@ -4,10 +4,9 @@ let i = 0, sum = 0, count = 0
 
 while(true) {
     if(input[i]>=30) {
-        console.log((sum/count).toFixed(2))
+        console.log((sum/i).toFixed(2))
         break
     }
     sum += input[i]
     i++
-    count++
 }
