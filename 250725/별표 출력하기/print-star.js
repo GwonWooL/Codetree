393609a7f1d3b712b6652b30d2b318c3ae37e30e
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 17:05:03 +0900

    Title: 별표 출력하기, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

--- 250725/별표 출력하기/print-star.js
+++ 250725/별표 출력하기/print-star.js
@@ -3,7 +3,14 @@ let n = Number(fs.readFileSync(0).toString())
 
 for(let i = 0; i<n; i++) {
     let star =""
-    for(let j = i; j<n; j++) {
+    for(let k = 0; k<=i; k++) {
+        star+="* "
+    }
+    console.log(star)
+}
+for(let i = n-1; i>0; i--) {
+    let star = ""
+    for(let k = i; k>0; k--) {
         star+="* "
     }
     console.log(star)
