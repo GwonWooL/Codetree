25fd62e68de67b917c76d2ad80056cdcb6f546c2
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 15:54:56 +0900

    Title: ab 사이에 없는 c, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250725/ab 사이에 없는 c/c-between-a-and-b-2.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+let input = fs.readFileSync(0).toString().split(" ").map(Number)
+let result = false
+
+for(let i = input[0]; i<=input[1]; i++) {
+    if(i%input[2]==0) {
+        result = true
+    }
+}
+console.log(result==false ? "YES":"NO")
