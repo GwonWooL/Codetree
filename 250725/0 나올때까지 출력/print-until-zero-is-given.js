63b9b9f9b8484feac42453748fa026e2b83d7cf8
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 14:11:53 +0900

    Title: 0 나올때까지 출력, Time: 181ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250725/0 나올때까지 출력/print-until-zero-is-given.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+let input = fs.readFileSync(0).toString().split("\n").map(Number)
+let i = 0
+
+while(true) {
+    if(input[i]!=0) {
+        console.log(input[i])
+    }
+    else {
+        break
+    }
+    i++
+}
