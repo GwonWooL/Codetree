80f9a8c0d1babb84afa80d05a1af65b7c6a6d4a3
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 19:26:54 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 199ms, Memory: 47MB, Status: Passed - Codetree

---
+++ 250725/특정 규칙에 따른 문자 출력/character-output-according-to-rule.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+let n = Number(fs.readFileSync(0).toString())
+
+for(let i = 0; i<n; i++) {
+    let star = ""
+    for(let j = i; j<n-1; j++) {
+        star+="  "
+    }
+    for(let k = 0; k<=i; k++) {
+        star+="@ "
+    }
+    console.log(star)
+}
+for(let i = n-1; i>0; i--) {
+    let star = ""
+    for(let j = i; j>0; j--) {
+        star+="@ "
+    }
+    console.log(star)
+}
