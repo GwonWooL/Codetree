bbd0b4be39fb4fd24475b29e541c806be2dbe17d
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 19:14:01 +0900

    Title: 별 그리기, Time: 185ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250725/별 그리기/star-drawing.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+let n = Number(fs.readFileSync(0).toString())
+
+for(let i = 1; i<=n; i++) {
+    let star = ""
+    for(let j = i; j<n; j++) {
+        star+=" "
+    }
+    for(let k = 0; k<2*i-1; k++) {
+        star+="*"
+    }
+    console.log(star)
+} 
+for(let i = n; i>1; i--) {
+    let star = ""
+    for(let j = i; j<=n; j++) {
+        star+=" "
+    }
+    for(let k = 2*i-1; k>=n; k--) {
+        star+="*"
+    }
+    console.log(star)
+}
