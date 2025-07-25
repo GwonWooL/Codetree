751ab4c6c20193b923ce9ea2269d3ed1c57a31e8
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-25 14:50:35 +0900

    Title: 20대의 평균 나이, Time: 5091ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

--- 250725/20대의 평균 나이/average-age-of-20.js
+++ 250725/20대의 평균 나이/average-age-of-20.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 let input = fs.readFileSync(0).toString().split("\n").map(Number)
-let i = 0, sum = 0, count = 0
+let i = 0, sum = 0
 
 while(true) {
     if(input[i]>=30) {
