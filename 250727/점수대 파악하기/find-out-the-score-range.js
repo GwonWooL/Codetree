630cd550c51ac4db66f16e6728942bdf45b961d8
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-27 01:28:40 +0900

    Title: 점수대 파악하기, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

--- 250727/점수대 파악하기/find-out-the-score-range.js
+++ 250727/점수대 파악하기/find-out-the-score-range.js
@@ -3,7 +3,11 @@ let arr = fs.readFileSync(0).toString().split(" ").map(Number)
 let countArr = Array(11).fill(0)
 
 for(let i = 0; i<arr.length; i++) {
+    if(arr[i]==0) {
+        break
+    }
     countArr[parseInt(arr[i]/10)]++
+  
 }
 for(let i = 10; i>=1; i--) {
     console.log(i*10+" - "+countArr[i])
