2ef8f171c6a1b0cad3293bc5815ced40cd4cf40b
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-07-26 19:50:58 +0900

    Title: 학점 계산기, Time: 162ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250726/학점 계산기/credit-calculator.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+let arr = fs.readFileSync(0).toString().split("\n")
+let n = Number(arr[0])
+let input = arr[1].split(" ").map(Number)
+let sum = 0;
+
+for(let i = 0; i<n; i++) {
+    sum += input[i]
+}
+let avg = sum/n
+
+if(avg>=4.0) {
+    console.log(`${avg.toFixed(1)}\nPerfect`)
+}
+else if(avg>=3.0) {
+    console.log(`${avg.toFixed(1)}\nGood`)
+}
+else {
+    console.log(`${avg.toFixed(1)}\nPerfect`)
+}
